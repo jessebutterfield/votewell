4a7aaf864945efc5ee59660a3636b7e2b40be8b9
jessebutterfield <jessebutterfield@465ddf34-cc00-3b3b-d79e-78db1a172c02>
2011-12-06 05:25:02 +0000

    Wrote parsing script for adding people.xml file from GovTrack.us, added state view for seeing legislators from said state.

--- voteWell/votes/views.py
+++ voteWell/votes/views.py
@@ -1,1 +1,11 @@
-# Create your views here.
+from django.template import Context, loader
+from votes.models import Legislator
+from django.http import HttpResponse
+
+def state(request,state_abbr):
+    legs = Legislator.objects.filter(state=state_abbr)
+    t = loader.get_template('votes/state.html')
+    c = Context({
+        'legs': legs,
+    })
+    return HttpResponse(t.render(c))
