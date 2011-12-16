7ad0be2cffb0a1140ffc2539e64e1c9767f8dce0
jessebutterfield <jessebutterfield@465ddf34-cc00-3b3b-d79e-78db1a172c02>
2011-12-16 23:36:11 +0000

    Added commenting, and rolls, starting to get there. Lots more testing necessary.

--- trunk/voteWell/votes/admin.py
+++ trunk/voteWell/votes/admin.py
@@ -1,4 +1,8 @@
-from votes.models import Legislator
+from votes.models import Legislator, Subject, Bill, Roll, Vote
 from django.contrib import admin
 
 admin.site.register(Legislator)
+admin.site.register(Subject)
+admin.site.register(Bill)
+admin.site.register(Roll)
+admin.site.register(Vote)
