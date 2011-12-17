8a29c350031f2f537706fd92729f5744106285e9
jessebutterfield <jessebutterfield@465ddf34-cc00-3b3b-d79e-78db1a172c02>
2011-12-17 01:25:19 +0000

    Fixed everything to run with older version of django

---
+++ voteWell2/votes/models.py
@@ -0,0 +1,127 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Bill(models.Model):
+    session = models.IntegerField()
+    kind = models.CharField(max_length=10)
+    number = models.IntegerField()
+    mainTitle = models.CharField(max_length=100)
+    updated = models.DateTimeField()
+    state_datetime = models.DateTimeField()
+    state = models.CharField(max_length=20)
+    introduced = models.DateField()
+    sponsor = models.ForeignKey('Legislator')
+    cosponsors = models.ManyToManyField('Legislator',related_name='cosponsored')
+    summary = models.TextField()
+    relatedbills = models.ManyToManyField('self',through='Relationship',symmetrical=False)
+    subjects = models.ManyToManyField('Subject')
+    def __unicode__(self):
+        return str(self.session)+'--'+self.kind+str(self.number)
+    
+
+class Legislator(models.Model):
+    lastname = models.CharField(max_length=20)
+    firstname = models.CharField(max_length=20)
+    middlename = models.CharField(max_length=20)
+    birthday = models.DateField(null=True,blank=True)
+    gender = models.CharField(max_length=1)
+    religion = models.CharField(max_length=20)
+    pvsid = models.IntegerField(null=True,blank=True)
+    osid = models.CharField(max_length=20)
+    bioguideid = models.CharField(max_length=20)
+    metavidid = models.CharField(max_length=20)
+    youtubeid = models.CharField(max_length=20)
+    icpsrid = models.IntegerField(null=True,blank=True)
+    facebookgraphid = models.IntegerField(null=True,blank=True)
+    name = models.CharField(max_length=100)
+    title = models.CharField(max_length=30)
+    state = models.CharField(max_length=2)
+    district = models.IntegerField(null=True,blank=True)
+    party = models.CharField(max_length=10)
+    position = models.CharField(max_length=10)
+    url = models.CharField(max_length=20)
+    def __unicode__(self):
+        return self.name
+    
+class Roll(models.Model):
+    category = models.CharField(max_length=20)
+    kind = models.CharField(max_length=20)
+    question = models.CharField(max_length=50)
+    bill = models.ForeignKey(Bill,null=True,blank=True)
+    where = models.CharField(max_length=20)
+    session = models.IntegerField()
+    year = models.IntegerField()
+    number = models.IntegerField()
+    source = models.CharField(max_length=20)
+    when = models.DateTimeField()
+    updated = models.DateTimeField()
+    aye = models.IntegerField()
+    nay = models.IntegerField()
+    novote = models.IntegerField()
+    present = models.IntegerField()
+    keys = models.ManyToManyField('VoteKey')
+    required = models.CharField(max_length=5)
+    result = models.CharField(max_length=10)
+    votes = models.ManyToManyField(Legislator,through='Vote')
+    def __unicode__(self):
+        return self.question
+    
+class Vote(models.Model):
+    roll = models.ForeignKey(Roll)
+    voter = models.ForeignKey(Legislator)
+    vote = models.CharField(max_length=1)
+    argument = models.TextField()
+    
+class VoteKey(models.Model):
+    symbol = models.CharField(max_length=1)
+    vote = models.CharField(max_length=50)
+    def __unicode__(self):
+        return self.symbol+'='+self.vote    
+    
+
+class Title(models.Model):
+    bill = models.ForeignKey(Bill)
+    kind = models.CharField(max_length=15)
+    how = models.CharField(max_length=15)
+    title = models.CharField(max_length=100)
+    def __unicode__(self):
+        return self.title
+    
+class Subject(models.Model):
+    name = models.CharField(max_length=50)
+    def __unicode__(self):
+        return self.name
+    
+class Committee(models.Model):
+    code = models.CharField(max_length=10)
+    name = models.CharField(max_length=50)
+    bills = models.ManyToManyField(Bill,through='Activity')
+    def __unicode__(self):
+        return self.name
+    
+class Relationship(models.Model):
+    srcBill = models.ForeignKey(Bill)
+    destBill = models.ForeignKey(Bill,related_name='reverseRelated')
+    kind = models.CharField(max_length=15)
+    
+class Activity(models.Model):
+    committee = models.ForeignKey(Committee)
+    bill = models.ForeignKey(Bill)
+    activity = models.CharField(max_length=30)
+    
+class Comment(models.Model):
+    user = models.ForeignKey(User)
+    created = models.DateTimeField()
+    bill = models.ForeignKey(Bill,null=True,blank=True)
+    roll = models.ForeignKey(Roll,null=True,blank=True)
+    sentiment = models.CharField(max_length=1)
+    text = models.TextField()
+
+class UserProfile(models.Model):
+    # This field is required.
+    user = models.OneToOneField(User)
+    follows = models.ManyToManyField(User,related_name="followers")
+    legislators = models.ManyToManyField(Legislator)
+    
+
+    
---
+++ voteWell2/votes/views.py
@@ -0,0 +1,76 @@
+from django.template import Context, loader, RequestContext
+from django.shortcuts import render_to_response, get_object_or_404
+from django.db.models import Count
+from votes.models import Legislator, Bill, Comment
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
+from datetime import datetime
+
+def home(request):
+    if(request.user.is_authenticated()):
+        t = loader.get_template('votes/home.html')
+        comments = request.user.comment_set.all().order_by('-created')[:5]
+        c = Context({'user': request.user,
+                     'comments': comments})
+    else:
+        t = loader.get_template('welcome.html')
+        c = Context({})
+    return HttpResponse(t.render(c))
+
+def state(request,state_abbr):
+    legs = Legislator.objects.filter(state=state_abbr)
+    t = loader.get_template('votes/state.html')
+    c = Context({
+                 'legs': legs,
+    })
+    return HttpResponse(t.render(c))
+
+    
+
+def legislatorDetail(request,leg_id):
+    leg = Legislator.objects.get(pk=leg_id)
+    bills = leg.bill_set.all()
+    t = loader.get_template('votes/legislator.html')
+    c = Context({
+                 'leg': leg,
+                 'bills': bills,})
+    return HttpResponse(t.render(c))
+
+def billDetail(request,bill_id):
+    bill = Bill.objects.get(pk=bill_id)
+    votes = []
+    if(bill.roll_set.count()>0):
+        roll = bill.roll_set.all()[0]
+        votes = roll.vote_set.all()
+    else:
+        roll = []
+    comments = bill.comment_set.annotate(Count("sentiment")).order_by("sentiment","-created")
+    numAye = 0
+    numNay = 0
+    if(len(comments)>0):
+        if(comments[0].sentiment == '+'):
+            numAye = comments[0].sentiment__count
+        if(comments[numAye].sentiment == '-'):
+            numNay = comments[numAye].sentiment__count
+    commentMatrix = [comments[0:min(numAye,5)],comments[numAye:numAye+min(numNay,5)],
+                     comments[numAye+numNay:numAye+numNay+5]]    
+    voteMatrix = [[],[],[],[]]
+    voteOptions = ['+','-','P','0']
+    for vote in votes:
+        voteMatrix[voteOptions.index(vote.vote)].append(vote)   
+        
+    c = {'bill': bill,
+         'roll': roll,
+         'voteMatrix': voteMatrix,
+         'commentMatrix': commentMatrix,}
+    return render_to_response('votes/bill.html', c,context_instance=RequestContext(request))
+
+
+def billComment(request,bill_id):
+    b = get_object_or_404(Bill, pk=bill_id)
+    sentiment = request.POST['comment']
+    text = request.POST['commentText']
+    c = Comment(bill=b,user=request.user,text=text,sentiment=sentiment,created=datetime.now())
+    c.save()
+    return HttpResponseRedirect(reverse('votes.views.billDetail', args=(b.id,)))
+
